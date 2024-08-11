00544b98ba94b78ba1b84b358ced6271aaa82d1b
TJ Dickerson <tjdickerson@gmail.com>
2024-08-10 21:54:48 -0500

    Basic set up.

--- internal/sacdb/sacdb.go
+++ internal/sacdb/sacdb.go
@@ -1,8 +1,9 @@
-package database
+package sacdb
 
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	sqlite3 "github.com/mattn/go-sqlite3"
 	"log"
 	"os"
@@ -17,11 +18,69 @@ func GetDatabase() (*sql.DB, error) {
 	_, existErr := os.Stat(DB_PATH)
 	if errors.Is(existErr, os.ErrNotExist) {
 		log.Printf("Database not found. Initializing database...\n")
-		return createSchema(DB_PATH)
+		tdb, cErr := createSchema(DB_PATH)
+		if cErr != nil {
+			log.Fatal(fmt.Sprintf("Couldn't create the database: %s\n", cErr))
+		}
+		tdb.Close()
 	}
 
 	log.Printf("Loading database...\n")
-	return sql.Open("sqlite3", DB_PATH)
+	db, err := sql.Open("sqlite3", DB_PATH+"?cache=shared")
+	db.SetMaxOpenConns(1)
+	return db, err
+}
+
+func HasAccount(db *sql.DB) bool {
+	result, err := db.Query("select count(1) from accounts;")
+	if err != nil {
+		log.Printf("Failed to find accounts table.\n")
+		log.Printf("  %s\n", err)
+		return false
+	}
+
+	var count int32 = 0
+	if result.Next() {
+		result.Scan(&count)
+	}
+
+	result.Close()
+
+	return count > 0
+}
+
+func GetDefaultAccount(db *sql.DB) string {
+	result, err := db.Query("select name from accounts where id = 1")
+	defer result.Close()
+	if err != nil {
+		log.Printf("Failed to find account...\n")
+		return "UNKNOWN"
+	}
+
+	var account string
+	if result.Next() {
+		result.Scan(&account)
+	}
+
+	return account
+}
+
+func CreateNewAccount(db *sql.DB, name string) error {
+	stmt, err := db.Prepare("insert into accounts(name) values(?);")
+	if err != nil {
+		log.Printf("Couldn't prepare new account.\n")
+		return err
+	}
+
+	log.Printf("About to insert the thing...\n")
+
+	_, err = stmt.Exec(name)
+	if err != nil {
+		log.Printf(fmt.Sprintf("Couldn't create new account: %s\n", err))
+		return err
+	}
+
+	return nil
 }
 
 func createSchema(db_path string) (*sql.DB, error) {
@@ -31,10 +90,53 @@ func createSchema(db_path string) (*sql.DB, error) {
 		return nil, err
 	}
 
-	var testTable string = "create table if not exists test (id integer primary key, name varchar(50))"
-
-	statement, err := db.Prepare(testTable)
-	statement.Exec()
+	createTable(db, CT_ACCOUNT)
+	createTable(db, CT_CATEGORIES)
+	createTable(db, CT_LEDGER)
 
 	return db, nil
 }
+
+func createTable(db *sql.DB, statement string) error {
+
+	stmt, err := db.Prepare(statement)
+	if err != nil {
+		log.Printf("Failure preparing statement: %s\n", err)
+		return err
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Printf("Failure executing statement: %s\n", err)
+		return err
+	}
+
+	return nil
+}
+
+const CT_ACCOUNT = `
+	create table if not exists accounts (
+		id integer primary key,
+	    name varchar(100)
+	);
+	`
+
+const CT_CATEGORIES = `
+	create table if not exists categories (
+		id integer primary key,
+	    name varchar(100)
+	);
+	`
+
+const CT_LEDGER = `
+	create table if not exists ledger (
+		id integer primary key,
+		trancsaction_date integer,
+		amount integer,
+	    name varchar(1000),
+	    account_id integer,
+	    category_id integer,
+	    foreign key(account_id) references accounts(id),
+	    foreign key(category_id) references categories(id)
+	);
+`
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	sacdb "internal/sacdb"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -15,4 +20,64 @@ func main() {
 	}
 
 	defer db.Close()
+
+	var accountName string
+	if !sacdb.HasAccount(db) {
+		log.Printf("You have no accounts configured.\n")
+
+		accountName = getStringFromUser("Enter name for account: ")
+		accountName = strings.TrimSpace(accountName)
+		log.Printf("Creating (%s) account.\n", accountName)
+
+		err = sacdb.CreateNewAccount(db, accountName)
+		if err != nil {
+			log.Printf("Failed to create account: %s\n", err)
+		} else {
+			log.Printf("Account created.\n")
+		}
+	} else {
+		accountName = sacdb.GetDefaultAccount(db)
+	}
+
+	if err == nil {
+		clear()
+		displayMenu(accountName)
+	}
+}
+
+func clear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func displayMenu(accountName string) {
+	fmt.Printf("%s\n", headerRow(accountName))
+}
+
+func headerRow(accountName string) string {
+	currentTime := time.Now()
+	var header string
+
+	title := fmt.Sprintf("sacmoney - %s", accountName)
+	date := currentTime.Format("Mon  02 Jan 2006")
+
+	headLen := 80
+	space := headLen - len(title) - len(date)
+	spacer := strings.Repeat(" ", space)
+
+	header = fmt.Sprintf("%s%s%s", title, spacer, date)
+
+	return header
+}
+
+func getStringFromUser(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("%s", prompt)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Failed to read input string.")
+	}
+
+	return input
 }
